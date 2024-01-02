8998ccb872f9a089b541855ca3d312350bea1331
jpalaciosdedUOC <jpalaciosded@uoc.edu>
2024-01-02 18:57:17 +0100

    Prueba

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,20 @@
-module com.onlinestore.onlinestore_producto5 {
+module com.onlinestore.onlinestore_producto {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires java.naming;
+    requires javaee.api;
 
     opens com.onlinestore.onlinestore_producto5 to javafx.fxml;
     exports com.onlinestore.onlinestore_producto5;
+    exports vista;
+    opens vista to javafx.fxml;
+    exports controlador;
+    opens controlador to javafx.fxml;
+    exports modelo;
+    opens modelo to org.hibernate.orm.core;
+
 }
