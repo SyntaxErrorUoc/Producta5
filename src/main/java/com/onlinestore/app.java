e85ddb06bb0b72e30225a94ae616129aa6477e63
ChusUOC <jjoyas@uoc.edu>
2024-01-02 19:17:29 +0100

    added app

---
+++ src/main/java/com/onlinestore/app.java
@@ -0,0 +1,11 @@
+package com.onlinestore;
+
+import com.onlinestore.vista.GestionOS;
+
+import static javafx.application.Application.launch;
+
+public class app {
+    public static void main(String[] args) {
+        launch(GestionOS.class);
+    }
+}
